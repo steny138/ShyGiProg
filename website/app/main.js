89789730ceb8eddcbc38bb1357507803197826f7
Yuchen Liu <steny138@gmail.com>
2016-03-25 01:35:05 +0800

    add provide http parameters

--- website/app/main.js
+++ website/app/main.js
@@ -1,7 +1,7 @@
-System.register(['angular2/platform/browser', './member.component', './services/MemberServices'], function(exports_1, context_1) {
+System.register(['angular2/platform/browser', './member.component', './services/MemberServices', 'angular2/core', 'angular2/http'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var browser_1, member_component_1, MemberServices_1;
+    var browser_1, member_component_1, MemberServices_1, core_1, http_1;
     return {
         setters:[
             function (browser_1_1) {
@@ -12,9 +12,15 @@ System.register(['angular2/platform/browser', './member.component', './services/
             },
             function (MemberServices_1_1) {
                 MemberServices_1 = MemberServices_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
             }],
         execute: function() {
-            browser_1.bootstrap(member_component_1.MemberComponent, [MemberServices_1.MemberService]);
+            browser_1.bootstrap(member_component_1.MemberComponent, [MemberServices_1.MemberService, core_1.provide(http_1.Http, { useClass: http_1.Http })]);
         }
     }
 });
--- website/app/main.ts
+++ website/app/main.ts
@@ -1,5 +1,7 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {MemberComponent} from './member.component';
 import {MemberService} from './services/MemberServices';
+import {provide} from 'angular2/core';
+import {Http} from 'angular2/http';
 
-bootstrap(MemberComponent, [MemberService]);
+bootstrap(MemberComponent, [MemberService, provide(Http, {useClass: Http})]);
